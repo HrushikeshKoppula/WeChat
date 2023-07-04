8c67a770bbff9d432db6588908757eb71077b432
SniperFox <hrushikeshkoppula@gmail.com>
2023-07-04 16:48:41 +0530

    damn my computer slow,added backend and entry msg

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
-import { useState,useEffect }from 'react'
+import { useState,useEffect }from 'react';
+import io from 'socket.io-client';
+const socket = io.connect('http://localhost:5000');
 
 function App() {
   const [ state,setState ] = useState('login');
   const [ username,setUsername ] = useState(null);
   const [ roomid,setRoomid ] = useState(null);
+  const loginAttempt = (e) => {
+      e.preventDefault();
+      socket.emit('login-attempt',{ username,roomid });
+      setState('chat');
+  }
+  socket.off('entry').on('entry',(data)=>{
+    if(data===username)
+    document.getElementById("entry").innerText="Welcome "+data;
+    else
+    document.getElementById("entry").innerText=data+" Joined";
+    setTimeout(()=>document.getElementById("entry").innerText='',4000);
+  });
+
+
   if(state==='login'){
   return (
     <div className="App">
@@ -12,12 +28,12 @@ function App() {
           <h1>WeChat</h1>
         </header>
         <main className="join-main">
-        <form onSubmit={()=>setState('chat')}>
+        <form onSubmit={loginAttempt}>
           <label htmlFor="username">Username :</label><br />
-          <input onChange={(e)=>setUsername(e.target.value)} type="text" id="username" placeholder="Enter username" required autocomplete="off" /><br /><br />
+          <input onChange={(e)=>setUsername(e.target.value)} type="text" id="username" placeholder="Enter username" required autoComplete="off" /><br /><br />
           <label htmlFor="roomname">Room:</label><br />
-          <input onChange={(e)=>setRoomid(e.target.value)} type="text" id="roomname" placeholder="Enter Roomid" required autocomplete="off" /><br /><br />
-          <input class="btn"type="submit" value="Join Room" />
+          <input onChange={(e)=>setRoomid(e.target.value)} type="text" id="roomname" placeholder="Enter Roomid" required autoComplete="off" /><br /><br />
+          <input className="btn"type="submit" value="Join Room" />
           <br /><br />
         </form>
         </main>
@@ -38,7 +54,9 @@ function App() {
             <h3>Groups</h3>
             <ul id="rooms" />
           </div>
-          <div className="chat-messages" />
+          <div className="chat-messages">
+            <p id="entry"></p>
+          </div>
         </main>
         <div className="chat-form-container">
           <form>
---
+++ server/server.js
@@ -0,0 +1,23 @@
+const app = require('express')();
+const server = require('http').Server(app);
+const cors = require('cors');
+app.use(cors());
+const io = require('socket.io')(server, {
+    cors: { origin: 'http://localhost:3000' }
+});
+const port = 5000;
+// rooms=[];
+io.on('connection',(socket)=>{
+    console.log('user came');
+    socket.on('login-attempt',(data)=>{
+        // if(rooms.indexOf(data.roomid)===-1)
+        // rooms.push(roomid);
+        socket.join(data.roomid);
+        io.sockets.in(data.roomid).emit('entry',data.username);
+    })
+    socket.on('disconnect',()=>{
+        console.log('user left');
+    })
+})
+
+server.listen(port,()=>console.log(`*${port}`))
